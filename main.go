6e1064312228a9bf2584c1eabb66daa3fb8a9dbd
cycateng <cyber.gcw@gmail.com>
2019-09-10 23:53:33 +0900

    認証機能を実装

--- main.go
+++ main.go
@@ -1,18 +1,25 @@
 package main
 
 import (
-	"net/http"
+	"webapp/routes"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	router := gin.Default()
-	router.LoadHTMLGlob("views/*.html")
+	router.LoadHTMLFiles("views/*.html")
 	router.Static("/assets", "./assets")
 
-	router.GET("/", func(ctx *gin.Context) {
-		ctx.HTML(http.StatusOK, "index.html", gin.H{})
-	})
+	user := router.Group("/user")
+	{
+		user.POST("/signup", routes.UserSignUp)
+		user.POST("/login", routes.UserLogIn)
+	}
+
+	router.GET("/", routes.Home)
+	route.GET("/login", routes.LogIn)
+	route.GET("/signup", routes.SignUp)
+	router.NoRoute(routes.NoRoute)
 
 	router.Run(":80")
 }
